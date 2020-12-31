68d6f4ab284f15979cba37c57ebe2bbce4bdc10e
symon <hesynote@gmail.com>
2020-12-31 21:20:26 +0800

    Add balance query for erc20 demo

--- examples/erc20/assembly/index.ts
+++ examples/erc20/assembly/index.ts
@@ -7,11 +7,22 @@ import {
 const STORE_KEY = (new Uint8Array(32)).fill(0);
 
 enum Method {
+  BalanceOf = 0x56e929b2,
   TotalSupply = 0xdcb736b5
 }
 
 function totalSupply(): Uint8Array {
   const data = Storage.get(STORE_KEY, 16);
+  if (data.length > 0) {
+    return data;
+  } else {
+    return (new Uint8Array(16)).fill(0);
+  }
+}
+
+function balanceOf(input: Uint8Array): Uint8Array {
+  const owner = input;
+  const data = Storage.get(owner, 16);
   if (data.length > 0) {
     return data;
   } else {
@@ -29,6 +40,10 @@ export function call(): void {
   switch (selector) {
     case Method.TotalSupply: {
       Contract.returnValue(totalSupply());
+      break;
+    }
+    case Method.BalanceOf: {
+      Contract.returnValue(balanceOf(input.subarray(4)));
       break;
     }
   }
