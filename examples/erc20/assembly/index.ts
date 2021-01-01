4e38f7223b17458a25872a102436912d791c0d09
symon <hesynote@gmail.com>
2021-01-01 11:19:33 +0800

    support transfer function for erc20 contract

--- examples/erc20/assembly/index.ts
+++ examples/erc20/assembly/index.ts
@@ -1,3 +1,4 @@
+import { u128 } from "as-bignum";
 import {
   Util,
   Storage,
@@ -8,6 +9,7 @@ const STORE_KEY = (new Uint8Array(32)).fill(0);
 
 enum Method {
   BalanceOf = 0x56e929b2,
+  Transfer = 0xfae3a09d,
   TotalSupply = 0xdcb736b5
 }
 
@@ -20,13 +22,32 @@ function totalSupply(): Uint8Array {
   }
 }
 
-function balanceOf(input: Uint8Array): Uint8Array {
+function balanceOf(params: Uint8Array): Uint8Array {
+  const input = params.subarray(4);
   const owner = input;
   const data = Storage.get(owner, 16);
   if (data.length > 0) {
     return data;
   } else {
     return (new Uint8Array(16)).fill(0);
+  }
+}
+
+function transfer(params: Uint8Array): void {
+  const input = params.subarray(4);
+  const owner = Contract.caller();
+  const dest = input.subarray(0, 32);
+  const value = u128.from(input.subarray(32, 48));
+
+  const data = Storage.get(owner, 16);
+  const ownerBalance = (data.length > 0) ? u128.from(data): u128.from(0);
+
+  const receiver = Storage.get(dest, 16);
+  const destBalance = (receiver.length > 0) ? u128.from(receiver): u128.from(0);
+
+  if (ownerBalance >= value) {
+    Storage.set(owner, (ownerBalance - value).toUint8Array());
+    Storage.set(dest, (destBalance + value).toUint8Array());
   }
 }
 
@@ -43,7 +64,11 @@ export function call(): void {
       break;
     }
     case Method.BalanceOf: {
-      Contract.returnValue(balanceOf(input.subarray(4)));
+      Contract.returnValue(balanceOf(input));
+      break;
+    }
+    case Method.Transfer: {
+      transfer(input);
       break;
     }
   }
