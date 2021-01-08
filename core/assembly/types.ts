b3f691a777d1a02bf1b9518197705449700c9b73
symon <hesynote@gmail.com>
2021-01-08 20:12:30 +0800

    Fix length offset error of AccountId generation

--- core/assembly/types.ts
+++ core/assembly/types.ts
@@ -4,31 +4,32 @@ import { Util } from "./util";
 
 export class AccountId implements Codec {
 	// The byte length of Account Address
-  public static readonly ADDRESS_LENGTH:i32 = 32;
+  public static readonly ADDRESS_LENGTH: i32 = 32;
 	// Bytes Array of the Account Address
-  private address: u8[];
+  private _address: u8[];
 
   constructor(bytes: u8[] = []) {
-    assert(bytes.length == AccountId.ADDRESS_LENGTH, "AccountId: must 32 bytes length");
-    this.address = new Array<u8>();
-    this.address = this.address.concat(bytes);
+    assert(bytes.length == AccountId.ADDRESS_LENGTH, "AccountId: must be 32 bytes length");
+    this._address = new Array<u8>();
+    this._address = this._address.concat(bytes);
   }
 
   getAddress(): u8[] {
-    return this.address;
+    return this._address;
   }
 
   encodedLength(): i32 {
-    return this.address.length;
+    return this._address.length;
   }
 
   toU8a(): u8[]{
-    return this.address;
+    return this._address;
   }
 
   populateFromBytes(bytes: u8[], index: i32 = 0): void {
-    this.address = new Array<u8>();
-    this.address = this.address.concat(bytes.slice(index, AccountId.ADDRESS_LENGTH));
+    assert(bytes.length - index == AccountId.ADDRESS_LENGTH, "AccountId: must be 32 bytes length");
+    this._address = new Array<u8>();
+    this._address = this._address.concat(bytes.slice(index, index + AccountId.ADDRESS_LENGTH));
   }
 
   eq(other: AccountId): bool {
