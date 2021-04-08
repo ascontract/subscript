83dd0eee31190e692e59458738ea646f17b642c8
symon <hesynote@gmail.com>
2021-04-08 10:36:12 +0800

    add collections package for contract state

---
+++ core/assembly/collections/hashmap.ts
@@ -0,0 +1,10 @@
+import { Storage } from "../env";
+
+/**
+ * A hash map built on top of contract raw storage which store
+ * mapping between keys and values
+ */
+export class HashMap {
+  constructor() {
+  }
+}
---
+++ core/assembly/collections/index.ts
@@ -0,0 +1,5 @@
+/**
+ * Collections package provide a series of storage mapping for contract state
+ */
+
+export * from "./hashmap";
