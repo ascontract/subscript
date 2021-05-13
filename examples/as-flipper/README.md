a161a1408be8616b028d143c446ca33ae9bea18e
symon <hesynote@gmail.com>
2021-05-13 16:01:30 +0800

    add as flipper contract package for example contracts

---
+++ examples/as-flipper/README.md
@@ -0,0 +1,17 @@
+# Flipper contract example with subscript library
+
+Simple contract of flipper example
+
+**Install dependencies:**
+
+```
+yarn install
+```
+
+**Build Wasm target**
+
+```
+yarn build
+```
+
+After build, you can find the target wasm file in ./build/flipper.wasm
---
+++ examples/as-flipper/assembly/index.ts
@@ -0,0 +1,51 @@
+import {
+  Util,
+  Storage,
+  Contract
+} from "@subscript/core";
+
+const STORE_KEY = (new Uint8Array(32)).fill(0);
+
+enum Method {
+  Get = 0x2f865bd9,
+  Flip = 0x633aa551,
+}
+
+@as_view
+function get(): Uint8Array {
+  const data = Storage.get(STORE_KEY, 1);
+  const status: u8 = data.length ? (new DataView(data.buffer)).getUint8(0) : 0;
+  return (new Uint8Array(1)).fill(status);
+}
+
+@as_external
+function flip(): bool {
+  const data = Storage.get(STORE_KEY, 1);
+  const status: u8 = data.length ? (new DataView(data.buffer)).getUint8(0) : 0;
+  Storage.set(STORE_KEY, Util.toBytes(!status));
+  return true;
+}
+
+export function call(): void {
+  const input = Contract.input();
+  if (input.length < 4) {
+    return;
+  }
+
+  const selector = bswap(load<i32>(input.dataStart, 0));
+  switch (selector) {
+    case Method.Flip: {
+      flip();
+      break;
+    }
+    case Method.Get: {
+      Contract.returnValue(get());
+      break;
+    }
+  }
+  return;
+}
+
+export function deploy(): void {
+  return;
+}
